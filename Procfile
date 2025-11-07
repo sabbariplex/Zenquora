d8885cb0c99e35b64c850c6a52b5e798e5d5b689
sabbariplex <sabbar.iplex@gmail.com>
2025-11-07 11:37:53 -0500

    Use Gunicorn for production on Railway

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python run.py
+web: gunicorn --worker-class eventlet --workers 1 --threads 4 --timeout 120 --bind 0.0.0.0:$PORT --max-requests 1000 --max-requests-jitter 50 app:app
--- run.py
+++ run.py
@@ -6,5 +6,14 @@ from app import socketio, app
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_ENV') == 'development'
-    socketio.run(app, host='0.0.0.0', port=port, debug=debug, allow_unsafe_werkzeug=True)
+    
+    # For production, use gunicorn (configured in Procfile)
+    # For local development, use socketio.run
+    if os.environ.get('RAILWAY_ENVIRONMENT') or os.environ.get('RENDER'):
+        # Production: Use gunicorn (will be called from Procfile)
+        # This file is only used if Procfile is not present
+        socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
+    else:
+        # Local development
+        socketio.run(app, host='0.0.0.0', port=port, debug=debug, allow_unsafe_werkzeug=True)
 
